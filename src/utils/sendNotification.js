7b18da9bd06f63032d89e74dc3ff9fd65ec30d30
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-05-08 18:32:54 +0530

    added a .env file with some information

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,6 +1,19 @@
 const Notification = require("../model/notification.Schema");
 const admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config(); // Load .env variables
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -11,18 +24,16 @@ if (!admin.apps.length) {
 exports.sendNotification = async (data) => {
   try {
     const notificationCreated = await Notification.create(data);
-    io.emit("notificationCreated", {
-      message: "A New Notification Added",
-    });
+    
     const message = {
       notification: {
         title: data?.title || "Default Title",
         body: data?.subTitle || "Default Body",
         image: data?.icon || "Default Body",
       },
-      token:
-        "fCBfyfuaAl0FeG6e93S5mc:APA91bEWMG6tNIshaebx07iOP3lD537F-QOdgn_Wcl7unSBhjeuUzLNnUZccLDdbjb9ff-hg47alk9rJT-9bNYK_AwGaaGknvXgAgyMfkuo090qOjfEfTys",
+      token: "YOUR_FCM_DEVICE_TOKEN_HERE", // Replace or pass dynamically
     };
+
     const response = await admin.messaging().send(message);
     return notificationCreated;
   } catch (error) {
